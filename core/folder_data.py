1ffe8cfb64650f3720631b1553482cafd5af8405
Danylo Shevchuk <danny.sommar@gmail.com>
2025-07-14 12:34:37 +0100

    maybe a start

---
+++ core/folder_data.py
@@ -0,0 +1,4 @@
+class FolderData:
+    def __init__(self, path):
+        self.path = path
+        self.categories = []
---
+++ core/image_data.py
@@ -0,0 +1,4 @@
+class ImageData:
+    def __init__(self, path):
+        self.path = path
+        self.category = ""
